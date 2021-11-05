8fca1d43d99307ba8b4caab8fc98155ba8487adc
camila <camila3467@outlook.com>
2021-11-05 19:15:22 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,11 +8,29 @@ import NavBar from "components/NavBar";
 function App() {
   return (
     <>
-    <NavBar/>
-    <div className="container"> <h1 className="text-primary">ola mundo</h1>
-    </div>
+   <NavBar/>
+    <div className="container">
+      <h1 className="text-primary py-3">Dashboard de vendas</h1>
+  
+      <div className="row px-3">
+      <div className="col-sm-6">
+      <h5 className="text-center  text-secondary">Taxa de sucesso (%)</h5>
+        
+      
+        <BarCart/>
+        </div>
+        <div className="col-sm-6">
+        
+        <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+        <DonutChart/>
+        </div>
+ </div>
+ <div className="py-3">
+      <h2 className="text-primary">Todas as vendas </h2>
+   </div>
 
     <DataTable />
+    </div>
     <Footer />
   </>
     );
